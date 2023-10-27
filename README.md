8e45e055adea0daf5a862d38dff83785f1056a5d
iletenoux <seedspace6498@gmail.com>
2023-10-27 18:36:30 +0200

    feat(db): update pool size sql alchemy

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-readme
+Don't forget to create the necessary networks:
+docker network create --internal caddy_internal
+docket network create web
--- database.py
+++ database.py
@@ -4,9 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:root@db:3306/coverletterai"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=50, max_overflow=450)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
